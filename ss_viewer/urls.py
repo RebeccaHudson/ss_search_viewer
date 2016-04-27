73eb4d485c79e2b8fef6f6158de910c4f47a9f4e
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-04-27 23:58:33 +0100

    Add a url route for the search page. Add an app_name so I can namespace urls from within templates.

--- ss_viewer/urls.py
+++ ss_viewer/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'ss_viewer'
+
 urlpatterns = [
   url(r'^$', views.index, name = 'index'),
   #url(r'one-snp-detail/$', views.one_snp_detail, name = 'one-snp-detail'),
   url(r'one-snp-detail/rs(?P<snpid_numeric>[0-9]+)/$', views.one_snp_detail, name = 'one-snp-detail'), 
-  url(r'^$', views.index, name = 'one-snp-detail'),
+  url(r'search/$', views.get_scores_for_list, name = 'search'),
 ]
