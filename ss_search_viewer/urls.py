aca4efaeb9b741a78db8a33e7c0065cb32b006e8
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-04-27 23:57:33 +0100

    Add a slash to the end of the application's root url.

--- ss_search_viewer/urls.py
+++ ss_search_viewer/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^ss_viewer', include('ss_viewer.urls')),
+    url(r'^ss_viewer/', include('ss_viewer.urls')),
     url(r'^admin/', admin.site.urls),
 ]
