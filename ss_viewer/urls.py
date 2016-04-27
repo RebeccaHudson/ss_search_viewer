3e8d2755f77594b3a9a86d5068cbfe4d00c41a67
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-04-27 18:43:46 +0100

    Add a view to see data for one snpid specified in the url. Eg: /ss_viewer/one-snp-detail/rs576026753

--- ss_viewer/urls.py
+++ ss_viewer/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index, name = 'index'),
+  #url(r'one-snp-detail/$', views.one_snp_detail, name = 'one-snp-detail'),
+  url(r'one-snp-detail/rs(?P<snpid_numeric>[0-9]+)/$', views.one_snp_detail, name = 'one-snp-detail'), 
+  url(r'^$', views.index, name = 'one-snp-detail'),
 ]
--- ss_viewer/views.py
+++ ss_viewer/views.py
@@ -1,7 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+import json
 
+#found at the URL: ss_viewer
 def index(request):
   return HttpResponse("This is a basic response")
 
-# Create your views here.
+
+
+#returns one snp from the API
+#specify the snpid in the URL
+def one_snp_detail(request, snpid_numeric):
+  
+  #the requests library probably has a cleaner, happier way to do this.
+  #hardurl = 'http://0.0.0.0:8002/api_v0/one-scores-snpid/rs571194783'
+  api_host_url = 'http://0.0.0.0'
+  api_host_port = '8005'
+  host_w_port = [ api_host_url, api_host_port]
+
+  api_root_url = 'api_v0'
+  api_function = 'one-scores-snpid'
+  snpid = 'rs' + snpid_numeric
+  url_args = [ api_root_url, api_function, snpid ]
+
+  host_w_port = ':'.join(host_w_port)
+  url_args = '/'.join(url_args)
+  url = host_w_port + "/" + url_args + "/" 
+
+  print("url = " + url) 
+  
+  r = requests.get(url)
+  return HttpResponse(r.text)
+
+
+def get_snp_list(request):
+  pass
+
+
+
+
