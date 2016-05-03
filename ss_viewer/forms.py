ff34beb93820d0b6bdd55d51bba92daf20ff173e
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-05-03 21:26:39 +0100

    Put some really simple fake snpids in the intial load of the rsnpid search text area to streamline whatever manaul testing I have to do.

--- ss_viewer/forms.py
+++ ss_viewer/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 
 class ScoresSearchForm(forms.Form):
+   some_fake_snpids = "rs376201521  rs762269735   rs376019116"
    raw_requested_snpids = forms.CharField(widget=forms.Textarea,
-                                          max_length=100000, strip=True)
+                                    max_length=100000,
+                                    strip=True,
+                                    required=True,
+                                    initial=some_fake_snpids)
    #OTHER FIELDS WILL APPEAR HERE...
