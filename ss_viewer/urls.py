af34367f115f6addfa76f38ae4d3321214276681
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-05-03 20:18:26 +0100

    Replace the url rule for the view that points to searchpage.html with one that points to alt-searchpage.html. Note that the name had to be changed significantly for the new view to be loaded. It's unclear to me why this is the case.

--- ss_viewer/urls.py
+++ ss_viewer/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
   url(r'^$', views.index, name = 'index'),
   #url(r'one-snp-detail/$', views.one_snp_detail, name = 'one-snp-detail'),
   url(r'one-snp-detail/rs(?P<snpid_numeric>[0-9]+)/$', views.one_snp_detail, name = 'one-snp-detail'), 
-  url(r'search/$', views.get_scores_for_list, name = 'search'),
+  #url(r'search/$', views.get_scores_for_list, name = 'search'),
+  url(r'search/$', views.xxx_get_scores_for_list, name = 'alt-search'),
 ]
