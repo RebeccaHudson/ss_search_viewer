8b28a2cb07864b9b55fb1c988c107884556410be
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-05-05 23:22:55 +0100

    Add a file field to the form, some explanatory label text for the form widget, as well as some comments describing needed validation.

--- ss_viewer/forms.py
+++ ss_viewer/forms.py
@@ -2,9 +2,14 @@ from django import forms
 
 class ScoresSearchForm(forms.Form):
    some_fake_snpids = "rs376201521  rs762269735   rs376019116"
+   text_to_explain_snpbox = "Enter snpids to lookup scores data for"
    raw_requested_snpids = forms.CharField(widget=forms.Textarea,
                                     max_length=100000,
                                     strip=True,
                                     required=True,
+                                    label=text_to_explain_snpbox,
                                     initial=some_fake_snpids)
+   file_of_snpids = forms.FileField(required=False) #standard everything
+   #validator changes will be needed to reflect the fact that 1 of these
+   #must be specified, but I'll spit if you try to supply both.
    #OTHER FIELDS WILL APPEAR HERE...
