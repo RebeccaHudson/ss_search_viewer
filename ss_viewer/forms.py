3e02e0ef2cde487ee2d2e7ffa438171504869bed
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-05-06 19:59:33 +0100

    Add a constraint to the search form that requires a user to enter SNPids, or a file specifying SNPids, but not both.

--- ss_viewer/forms.py
+++ ss_viewer/forms.py
@@ -6,10 +6,29 @@ class ScoresSearchForm(forms.Form):
    raw_requested_snpids = forms.CharField(widget=forms.Textarea,
                                     max_length=100000,
                                     strip=True,
-                                    required=True,
+                                    required=False,
                                     label=text_to_explain_snpbox,
                                     initial=some_fake_snpids)
    file_of_snpids = forms.FileField(required=False) #standard everything
    #validator changes will be needed to reflect the fact that 1 of these
    #must be specified, but I'll spit if you try to supply both.
    #OTHER FIELDS WILL APPEAR HERE...
+
+   #This is where we specify relationships for 1-or-the-other form inputs.
+   #A user can specify SNPids. EITHER with a file or with the textox.
+   def clean(self):
+     cleaned_data = super(ScoresSearchForm, self).clean()
+     print(str(cleaned_data))
+     snpid_file = cleaned_data.get('file_of_snpids')
+     snpid_textbox_contents = cleaned_data.get('raw_requested_snpids')
+
+     if (snpid_file and snpid_textbox_contents):
+       raise forms.ValidationError(('Specify snpids in the textbox,'
+                                   ' OR provide a file, not both.'),
+                                    code='too-many-inputs')
+     
+     if (not(snpid_file or snpid_textbox_contents)):
+       raise forms.ValidationError(('You must specify snpids in the textbox,'
+                                   ' OR provide a file.'),
+                                    code='missing-input')
+     return cleaned_data
