5c2b9dcff00f441ffcc038eb2f4e4d1d34a6763b
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-05-12 22:53:28 +0100

    Add a couple of URL routes for the new multi-search template/view that handles showing users a blank form when they make a GET request to one of the form POST views as well, and for the gl-region-search, which handles the POST method for the form submissions for genomic location searches.

--- ss_viewer/urls.py
+++ ss_viewer/urls.py
@@ -7,5 +7,7 @@ app_name = 'ss_viewer'
 urlpatterns = [
   url(r'^$', views.index, name = 'index'),
   url(r'one-snp-detail/rs(?P<snpid_numeric>[0-9]+)/$', views.one_snp_detail, name = 'one-snp-detail'), 
+  url(r'^multi-search/$', views.show_multisearch_page, name='multi-search'),
+  url(r'^gl-region-search/$', views.handle_search_by_genomic_location, name='gl-region-search'),
   url(r'search/$', views.get_scores_for_list, name = 'search'),
 ]
