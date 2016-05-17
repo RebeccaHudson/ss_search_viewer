e2b49bf33f16d228f5915a52157b0d62dd0b86e9
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-05-17 19:29:16 +0100

    Change the name of the view that handles searches to handle_search_by_snpid.

--- ss_viewer/urls.py
+++ ss_viewer/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
   url(r'one-snp-detail/rs(?P<snpid_numeric>[0-9]+)/$', views.one_snp_detail, name = 'one-snp-detail'), 
   url(r'^multi-search/$', views.show_multisearch_page, name='multi-search'),
   url(r'^gl-region-search/$', views.handle_search_by_genomic_location, name='gl-region-search'),
-  url(r'search/$', views.get_scores_for_list, name = 'search'),
+  url(r'snpid-search/$', views.handle_search_by_snpid, name = 'snpid-search'),
 ]
