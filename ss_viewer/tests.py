37aceae18099bb595eaff5a6b6682f9ea07656bf
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-05-19 19:57:57 +0100

    Clean up a bunch of comments in the tests file.

--- ss_viewer/tests.py
+++ ss_viewer/tests.py
@@ -26,38 +26,16 @@ class OneScoresRowViewDetailTests(TestCase):
     response = self.client.get(reverse('ss_viewer:index'))
     self.assertEqual(response.status_code, 200)
     
-#TODO: grab a couple of SNPs from the actual, live DB to avoid having to have
-#these hardcoded. It seems like many of these aren't in there?
+  #TODO: grab a couple of SNPs from the actual, live DB to avoid having to have
+  #these hardcoded. It seems like many of these aren't in there?
   def test_that_snp_detail_loads(self):
     #consider making it a random id?
     numb = 145599635 
     response = self.client.get(reverse('ss_viewer:one-snp-detail', args=(numb,)))
-    #print("context " + response.context)
-    #api_response = response.context.flatten().get('api_response') 
-    #print("The api told us this: " + str(api_response))
     response_json = json.loads(response.content)
-    print("response: " + str(response))
     self.assertEqual(len(response_json), 1)
-    # This is an unused oddball function. Should probably remove this.
-    #self.check_for_expected_fields_in_scores_row(response_json[0].keys())
-    print("response: " + str(response.content))
     self.assertEqual(response.status_code, 200)
-    #not sure what else is meaningful to test here, except that the views load.
  
-#  def test_that_scores_list_loads_post(self):
-#    response = self.client.post(reverse('ss_viewer:search'),
-#            { 'requested_snpids' :  '"rs376997626", "rs575624833", "rs189241347"]' })
-#    #Since the snpids are parsed out with a regular expression, we don't have to 
-#    #insist on a specific format..
-#    self.assertEqual(response.context.flatten().has_key('api_response'), True)
-#    api_response_data = response.context.flatten()['api_response']
-#    self.assertEqual(len(api_response_data), 3) #asked for 3 rows of data
-#    for data_row in api_response_data:
-#      fields_in_data_row = data_row.keys()
-#      self.check_for_expected_fields_in_scores_row(fields_in_data_row)
-#    self.assertEqual(response.status_code, 200)
-
-
   #we can only expect this test to pass if the requested snpids are actually in the database...
   #retain this code for testing when we actually expect matches to come out...
   def test_that_scores_list_loads_post(self):
@@ -84,14 +62,6 @@ class OneScoresRowViewDetailTests(TestCase):
     self.assertEqual(response.context.flatten().has_key('status_message'), True)
     self.assertEqual(response.context.flatten()['status_message'],'Invalid search. Try agian.')
     self.assertEqual(response.context['snpid_search_form'].is_valid(), False)
-    
-
-
-    #names of the fields in the form.
-    #response = self.client.post(reverse('ss_viewer:search'),
-    #        { 'requested_snpids' :  '"rs376997626", "rs575624833", "rs189241347"]' })
-
-
 
   def test_that_scores_list_loads_post_for_no_matching_snpids(self):
     response = self.client.post(reverse('ss_viewer:snpid-search'),
