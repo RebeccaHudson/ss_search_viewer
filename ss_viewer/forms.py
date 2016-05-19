3ba25c0f14e92e80540950a758c6c6408ad945c1
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-05-19 21:37:37 +0100

    Remove deprecated version, ScoresRowForm, of the form to search by snpid; all references should use SearchBySnpidForm.

--- ss_viewer/forms.py
+++ ss_viewer/forms.py
@@ -1,41 +1,6 @@
 from django import forms
 from django.conf import settings
 
-class ScoresSearchForm(forms.Form):
-   some_fake_snpids = "rs559407913  rs557153083 rs9431596"
-   text_to_explain_snpbox = "Enter snpids to lookup scores data for"
-   raw_requested_snpids = forms.CharField(widget=forms.Textarea,
-                                    max_length=100000,
-                                    strip=True,
-                                    required=False,
-                                    label=text_to_explain_snpbox,
-                                    initial=some_fake_snpids)
-   file_of_snpids = forms.FileField(required=False) #standard everything
-   #validator changes will be needed to reflect the fact that 1 of these
-   #must be specified, but I'll spit if you try to supply both.
-   #OTHER FIELDS WILL APPEAR HERE...
-
-   #This is where we specify relationships for 1-or-the-other form inputs.
-   #A user can specify SNPids. EITHER with a file or with the textox.
-   def clean(self):
-     cleaned_data = super(ScoresSearchForm, self).clean()
-     print(str(cleaned_data))
-     snpid_file = cleaned_data.get('file_of_snpids')
-     snpid_textbox_contents = cleaned_data.get('raw_requested_snpids')
-
-     #form.errors contains any errors that have come up by this point.
-
-     if (snpid_file and snpid_textbox_contents):
-       raise forms.ValidationError(('Specify snpids in the textbox,'
-                                   ' OR provide a file, not both.'),
-                                    code='too-many-inputs')
-     
-     if (not(snpid_file or snpid_textbox_contents)):
-       raise forms.ValidationError(('You must specify snpids in the textbox,'
-                                   ' OR provide a file.'),
-                                    code='missing-input')
-     return cleaned_data
-
 
 #This is just one way to specify what data you are looking for.
 #TODO: this should replacde the ScoresSearch form; it needs to be renamed...
