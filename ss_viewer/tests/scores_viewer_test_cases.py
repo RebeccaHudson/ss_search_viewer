771bc185927f5f541172f4cb47ebab61a249795b
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-05-25 23:06:18 +0100

    Add the trans_factor field to the list of expected fields to populate the part of the multi-search template that displays search results.

--- ss_viewer/tests/scores_viewer_test_cases.py
+++ ss_viewer/tests/scores_viewer_test_cases.py
@@ -5,7 +5,7 @@ import json
 
 class ScoresViewerTestCase(TestCase):
   def check_for_expected_fields_in_scores_row(self, data_fields_in_one_row):
-      expected_fields = [u'motif_len', u'snp_end', u'log_lik_ref', u'snpid', u'motif', u'log_lik_ratio', u'ref_strand', u'ref_start', u'snp_start', u'snp_strand', u'ref_end', u'log_enhance_odds', u'log_reduce_odds', u'log_lik_snp', u'pval_ref', u'pval_snp', u'pval_cond_ref', u'pval_cond_snp', u'pval_diff', u'pval_rank', u'chr', u'pos' ]
+      expected_fields = [u'motif_len', u'snp_end', u'trans_factor',  u'log_lik_ref', u'snpid', u'motif', u'log_lik_ratio', u'ref_strand', u'ref_start', u'snp_start', u'snp_strand', u'ref_end', u'log_enhance_odds', u'log_reduce_odds', u'log_lik_snp', u'pval_ref', u'pval_snp', u'pval_cond_ref', u'pval_cond_snp', u'pval_diff', u'pval_rank', u'chr', u'pos' ]
       # TODO this could be much better
       matching_fields = list(set(data_fields_in_one_row).intersection(expected_fields))
       self.assertEqual(len(expected_fields), len(matching_fields))
