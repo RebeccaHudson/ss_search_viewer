09d356f867934b23e29b9b1d3b08b6c96e81b9d1
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-06-13 23:38:40 +0100

    Add bootstrap's .form-control class to each form control widget in the attrs parameter.

--- ss_viewer/forms.py
+++ ss_viewer/forms.py
@@ -9,19 +9,22 @@ class SearchBySnpidForm(forms.Form):
    default_dummy_search = ("rs539483321, rs576894892, \
                             rs553761389, rs757299236, rs770590115")
    text_to_explain_snpbox = "Enter snpids to lookup scores data for"
-   raw_requested_snpids = forms.CharField(widget=forms.Textarea,
+   styled_widget = forms.Textarea(attrs={'class':'form-control'})
+   raw_requested_snpids = forms.CharField(widget=styled_widget,
                                     max_length=100000,
                                     strip=True,
                                     required=False,
                                     label=text_to_explain_snpbox,
-                                    initial=default_dummy_search)
+                                    initial=default_dummy_search,
+                                    )
    file_of_snpids = forms.FileField(required=False) #standard everything
-
    default_cutoff = 0.05
-   pvalue_rank_cutoff = forms.FloatField(required=False,
+   styled_widget = forms.NumberInput(attrs={'class':'form-control','step':"0.0001"})
+   pvalue_rank_cutoff = forms.FloatField(widget=styled_widget,
                                          max_value=1, 
                                          min_value=0, 
-                                         initial=default_cutoff)
+                                         initial=default_cutoff,
+                                         )
 
    def clean(self):
      cleaned_data = super(SearchBySnpidForm, self).clean()
@@ -51,27 +54,36 @@ class SearchByGenomicLocationForm(forms.Form):
 
   gl_pos_label_text = { 'start' : 'Start position on chromosome',
                         'end'   : 'End position on chromosome.' }
+
   # These defaults are here because I know there's development subset 
   # data in this range.
   default_start_pos = 10000;    default_end_pos = 10500
 
   # TODO: is there a way to put meaningful maximum values on these here?
-  gl_start_pos = forms.IntegerField(label = gl_pos_label_text['start'], 
+  styled_widget = forms.NumberInput(attrs={"class":'form-control','step':1})
+  gl_start_pos = forms.IntegerField(widget = styled_widget,
+                                    label = gl_pos_label_text['start'], 
                                     required = True,
                                     initial = default_start_pos,
-                                    min_value = 1 )
+                                    min_value = 0 )
 
-  gl_end_pos = forms.IntegerField(label = gl_pos_label_text['end'],
+  gl_end_pos = forms.IntegerField(widget = styled_widget,
+                                  label = gl_pos_label_text['end'],
                                   required = False, 
                                   initial = default_end_pos,
                                   min_value = 1)
 
   choices_for_chromosome =  ( ('ch1', 'ch1' ), ) 
-  selected_chromosome = forms.ChoiceField(choices=choices_for_chromosome,
+  styled_widget = forms.Select(attrs={"class":"form-control"})
+  selected_chromosome = forms.ChoiceField(widget=styled_widget,
+                                          choices=choices_for_chromosome,
                                           label="Select a chromosome.")
 
   default_cutoff = 0.05
-  pvalue_rank_cutoff = forms.FloatField(required=False,
+  
+  styled_widget = forms.NumberInput(attrs={'class':'form-control','step':"0.00001"})
+  pvalue_rank_cutoff = forms.FloatField(widget=styled_widget,
+                                         required=False,
                                          max_value=1, 
                                          min_value=0, 
                                          initial=default_cutoff)
@@ -91,7 +103,7 @@ class SearchByGenomicLocationForm(forms.Form):
 
       if start_pos > end_pos:
           raise forms.ValidationError(('Start position must be less than or equal'
-                                      'to the end position.'),
+                                      ' to the end position.'),
                                       code='region-size-0'  )
        
       max_size_of_region = settings.HARD_LIMITS['MAX_NUMBER_OF_SNPIDS_ALLOWED_TO_REQUEST']
@@ -123,18 +135,15 @@ class SearchByTranscriptionFactorForm(forms.Form):
 
     use_these_choices = sorted(tuple(use_these_choices))
    
-
-    #default_tf =  
-    trans_factor = forms.ChoiceField(choices = use_these_choices, 
+    styled_widget = forms.Select(attrs={"class":"form-control"})
+    trans_factor = forms.ChoiceField(widget = styled_widget,
+                                     choices = use_these_choices, 
                                      label = "Select a transcription factor.")
     default_cutoff = 0.05
-    pvalue_rank_cutoff = forms.FloatField(required=False,
+  
+    styled_widget = forms.NumberInput(attrs={'class':'form-control','step':"0.00001"})
+    pvalue_rank_cutoff = forms.FloatField(widget=styled_widget,
+                                           required=False,
                                            max_value=1, 
                                            min_value=0, 
                                            initial=default_cutoff)
-     
-
-
-
-
-                                     
