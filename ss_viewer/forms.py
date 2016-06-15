f142d937e8827ef8bc494013763ceb116718f719
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-06-15 14:33:33 -0500

    Add a hidden 'page_of_search_results_shown' field to each search form so the page number of the search results is available in the POST data of the form.

--- ss_viewer/forms.py
+++ ss_viewer/forms.py
@@ -6,87 +6,92 @@ import pickle
 #This is just one way to specify what data you are looking for.
 #TODO: this should replacde the ScoresSearch form; it needs to be renamed...
 class SearchBySnpidForm(forms.Form):
-   default_dummy_search = ("rs539483321, rs576894892, \
-                            rs553761389, rs757299236, rs770590115")
-   text_to_explain_snpbox = "Enter snpids to lookup scores data for"
-   styled_widget = forms.Textarea(attrs={'class':'form-control'})
-   raw_requested_snpids = forms.CharField(widget=styled_widget,
-                                    max_length=100000,
-                                    strip=True,
-                                    required=False,
-                                    label=text_to_explain_snpbox,
-                                    initial=default_dummy_search,
-                                    )
-   file_of_snpids = forms.FileField(required=False) #standard everything
-   default_cutoff = 0.05
-   styled_widget = forms.NumberInput(attrs={'class':'form-control','step':"0.0001"})
-   pvalue_rank_cutoff = forms.FloatField(widget=styled_widget,
-                                         max_value=1, 
-                                         min_value=0, 
-                                         initial=default_cutoff,
-                                         )
-
-   def clean(self):
-     cleaned_data = super(SearchBySnpidForm, self).clean()
-     print(str(cleaned_data))
-     snpid_file = cleaned_data.get('file_of_snpids')
-     snpid_textbox_contents = cleaned_data.get('raw_requested_snpids')
-
-     #form.errors contains any errors that have come up by this point.
-
-     if (snpid_file and snpid_textbox_contents):
-       raise forms.ValidationError(('Specify snpids in the textbox,'
-                                   ' OR provide a file, not both.'),
-                                    code='too-many-inputs')
-     
-     if (not(snpid_file or snpid_textbox_contents)):
-       raise forms.ValidationError(('You must specify snpids in the textbox,'
-                                   ' OR provide a file.'),
-                                    code='missing-input')
-     return cleaned_data
-
+    default_dummy_search = ("rs539483321, rs576894892, \
+                             rs553761389, rs757299236, rs770590115")
+    text_to_explain_snpbox = "Enter snpids to lookup scores data for"
+    styled_widget = forms.Textarea(attrs={'class':'form-control'})
+    raw_requested_snpids = forms.CharField(widget=styled_widget,
+                                     max_length=100000,
+                                     strip=True,
+                                     required=False,
+                                     label=text_to_explain_snpbox,
+                                     initial=default_dummy_search,
+                                     )
+    file_of_snpids = forms.FileField(required=False) #standard everything
+    default_cutoff = 0.05
+    styled_widget = forms.NumberInput(attrs={'class':'form-control','step':"0.0001"})
+    pvalue_rank_cutoff = forms.FloatField(widget=styled_widget,
+                                          max_value=1, 
+                                          min_value=0, 
+                                          initial=default_cutoff,
+                                          )
+    page_of_results_shown = forms.IntegerField(widget = forms.HiddenInput(), required = False)
+ 
+    def clean(self):
+        cleaned_data = super(SearchBySnpidForm, self).clean()
+        print(str(cleaned_data))
+        snpid_file = cleaned_data.get('file_of_snpids')
+        snpid_textbox_contents = cleaned_data.get('raw_requested_snpids')
+   
+        #form.errors contains any errors that have come up by this point.
+   
+        if (snpid_file and snpid_textbox_contents):
+            raise forms.ValidationError(('Specify snpids in the textbox,'
+                                        ' OR provide a file, not both.'),
+                                         code='too-many-inputs')
+        
+        if (not(snpid_file or snpid_textbox_contents)):
+            raise forms.ValidationError(('You must specify snpids in the textbox,'
+                                        ' OR provide a file.'),
+                                         code='missing-input')
+        return cleaned_data
+ 
 
 
 
 #A separate form for searching through the data by genomic location
 class SearchByGenomicLocationForm(forms.Form):
-  default_data = None   #don't need this.
+    default_data = None   #don't need this.
 
-  gl_pos_label_text = { 'start' : 'Start position on chromosome',
-                        'end'   : 'End position on chromosome.' }
+    gl_pos_label_text = { 'start' : 'Start position on chromosome',
+                          'end'   : 'End position on chromosome.' }
 
-  # These defaults are here because I know there's development subset 
-  # data in this range.
-  default_start_pos = 10000;    default_end_pos = 10500
+    # These defaults are here because I know there's development subset 
+    # data in this range.
+    default_start_pos = 10000;    default_end_pos = 10500
 
-  # TODO: is there a way to put meaningful maximum values on these here?
-  styled_widget = forms.NumberInput(attrs={"class":'form-control','step':1})
-  gl_start_pos = forms.IntegerField(widget = styled_widget,
-                                    label = gl_pos_label_text['start'], 
-                                    required = True,
-                                    initial = default_start_pos,
-                                    min_value = 0 )
+    # TODO: is there a way to put meaningful maximum values on these here?
+    styled_widget = forms.NumberInput(attrs={"class":'form-control','step':1})
+    gl_start_pos = forms.IntegerField(widget = styled_widget,
+                                      label = gl_pos_label_text['start'], 
+                                      required = True,
+                                      initial = default_start_pos,
+                                      min_value = 0 )
 
-  gl_end_pos = forms.IntegerField(widget = styled_widget,
-                                  label = gl_pos_label_text['end'],
-                                  required = False, 
-                                  initial = default_end_pos,
-                                  min_value = 1)
+    gl_end_pos = forms.IntegerField(widget = styled_widget,
+                                    label = gl_pos_label_text['end'],
+                                    required = False, 
+                                    initial = default_end_pos,
+                                    min_value = 1)
 
-  choices_for_chromosome =  ( ('ch1', 'ch1' ), ) 
-  styled_widget = forms.Select(attrs={"class":"form-control"})
-  selected_chromosome = forms.ChoiceField(widget=styled_widget,
-                                          choices=choices_for_chromosome,
-                                          label="Select a chromosome.")
+    choices_for_chromosome =  ( ('ch1', 'ch1' ), ) 
+    styled_widget = forms.Select(attrs={"class":"form-control"})
+    selected_chromosome = forms.ChoiceField(widget=styled_widget,
+                                            choices=choices_for_chromosome,
+                                            label="Select a chromosome.")
 
-  default_cutoff = 0.05
+    default_cutoff = 0.05
+    
+    styled_widget = forms.NumberInput(attrs={'class':'form-control','step':"0.00001"})
+    pvalue_rank_cutoff = forms.FloatField(widget=styled_widget,
+                                           required=False,
+                                           max_value=1, 
+                                           min_value=0, 
+                                           initial=default_cutoff)
   
-  styled_widget = forms.NumberInput(attrs={'class':'form-control','step':"0.00001"})
-  pvalue_rank_cutoff = forms.FloatField(widget=styled_widget,
-                                         required=False,
-                                         max_value=1, 
-                                         min_value=0, 
-                                         initial=default_cutoff)
+    page_of_results_shown = forms.IntegerField(widget = forms.HiddenInput(), required = False)
+  
+
    #ensure ranges are within hard limits.  
   def clean(self):
       cleaned_data = super(SearchByGenomicLocationForm, self).clean()
@@ -148,3 +153,4 @@ class SearchByTranscriptionFactorForm(forms.Form):
                                            min_value=0, 
                                            initial=default_cutoff)
     page_of_results_shown = forms.IntegerField(widget = forms.HiddenInput(), required = False)
+    #No clean method required because you can't mess this up? Or not so?
