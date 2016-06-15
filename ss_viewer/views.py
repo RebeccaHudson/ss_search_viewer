fa9ea8b200b68edf82e99f6232cda37c3ba36f81
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-06-15 15:46:44 -0500

    Fix the code that transorms motif values to transcription factor names such that when it attempts to lookup an unknown motif, UNKNOWN or something is shown, instead of an error showing up. Dict.get is cool. Also, further obfuscate the commit history by indenting a bunch of stuff to 4 spaces.

--- ss_viewer/views.py
+++ ss_viewer/views.py
@@ -40,17 +40,24 @@ def setup_api_url(api_function, snpid=None):
   return url
 
 def transform_motifs_to_transcription_factors(response_json):
-  lut = None
-  fpath = os.path.dirname(__file__) + "/lookup-tables" +\
-          '/lut_tfs_by_jaspar_motif.pkl'
-  with open(fpath , 'r') as f: 
-    lut = pickle.load(f) 
-  transformed_response = []
-  for one_row in response_json:
-    motif_value = one_row['motif']
-    one_row['trans_factor'] = lut[motif_value]
-    transformed_response.append(one_row) 
-  return transformed_response 
+    lut = None
+    fpath = os.path.dirname(__file__) + "/lookup-tables" +\
+            '/lut_tfs_by_jaspar_motif.pkl'
+    with open(fpath , 'r') as f: 
+        lut = pickle.load(f) 
+
+    transformed_response = []
+
+    for one_row in response_json:
+        motif_value = one_row['motif']
+        trans_factor = lut.get(motif_value)
+        if trans_factor is None:
+             one_row['trans_factor'] = "Not found."
+        else: 
+             one_row['trans_factor'] = trans_factor
+        transformed_response.append(one_row) 
+
+    return transformed_response 
 
 
 
@@ -162,56 +169,63 @@ def handle_search_by_snpid(request):
 #handle search by genomic location.. 
 #this should only handle POST
 def handle_search_by_genomic_location(request):
-  if request.method == 'GET':
-    return redirect(reverse('ss_viewer:multi-search'))
+    if request.method == 'GET':
+        return redirect(reverse('ss_viewer:multi-search'))
 
-  if request.method == 'POST': 
-    searchpage_template = 'ss_viewer/multi-searchpage.html'  
-    gl_search_form = SearchByGenomicLocationForm(request.POST)
-   
-    status_message = ""
-    if not gl_search_form.is_valid():
-       status_message = "Invalid search. Try agian."
-       return render(request, searchpage_template, 
-                        { 'gl_search_form'    : gl_search_form,
-                          'snpid_search_form' : SearchBySnpidForm(),
-                          'status_message'   : status_message     
-                        })
-    # may be able to unindent this:
-    if gl_search_form.is_valid():
-      form_data = gl_search_form.cleaned_data
-      specified_region = { 'chromosome' : form_data['selected_chromosome'],
-                           'start_pos'  : form_data['gl_start_pos'], 
-                           'end_pos'    : form_data['gl_end_pos'],
-                           'pvalue_rank': form_data['pvalue_rank_cutoff']   }
- 
-      api_response = requests.post( setup_api_url('search-by-gl'), 
-             json=specified_region, headers={ 'content-type' : 'application/json' })
- 
-      response_json = None
-      if api_response.status_code == 204:
-        status_message = "No matching rows"
-      elif api_response.status_code == 400:
-        status_message  = "API reported an error: " + api_response.text
-      else:
-        response_json = json.loads(api_response.text)
-        response_json = transform_motifs_to_transcription_factors(response_json)
-        status_message = 'Got ' + str(len(response_json)) + ' rows back from API.'
+    if request.method == 'POST': 
+        searchpage_template = 'ss_viewer/multi-searchpage.html'  
+        gl_search_form = SearchByGenomicLocationForm(request.POST)
+      
+        search_paging_info = None 
+        status_message = ""
+        response_data = None
 
-      # eventually remove: status_message += str(specified_region) 
-      new_form = SearchByGenomicLocationForm(form_data)
-      return render(request, searchpage_template, { 'api_response' : response_json,
-                                                    'tf_search_form' : SearchByTranscriptionFactorForm(),
-                                                    'gl_search_form': new_form, 
-                                                    'snpid_search_form' : SearchBySnpidForm(),
-                                                    'holdover_gl_region': specified_region,
-                                                    'active_tab'     : 'gl-region',
-                                                    'status_message' : status_message})                                              
+        if not gl_search_form.is_valid():
+             status_message = "Invalid search. Try agian."
+             return render(request, searchpage_template, 
+                              { 'gl_search_form'    : gl_search_form,
+                                'snpid_search_form' : SearchBySnpidForm(),
+                                'status_message'   : status_message,
+                                'active_tab'       : 'gl-region'
+                              })
+        form_data = gl_search_form.cleaned_data
+        search_request_params = get_paging_info_for_request(request, 
+                                                             form_data['page_of_results_shown'])
+        api_search_query = { 'chromosome' : form_data['selected_chromosome'],
+                             'start_pos'  : form_data['gl_start_pos'], 
+                             'end_pos'    : form_data['gl_end_pos'],
+                             'pvalue_rank': form_data['pvalue_rank_cutoff'],
+                             'from_result': search_request_params['search_result_offset'] }
+        api_response = requests.post( setup_api_url('search-by-gl'), 
+               json=api_search_query, headers={ 'content-type' : 'application/json' })
+
+        # DRY up the following part here, if it appears to be possible.
+        response_json = None
+        if api_response.status_code == 204:
+            status_message = "No matching rows"
+        elif api_response.status_code == 400:
+            status_message  = "API reported an error: " + api_response.text
+        else:
+            response_json = json.loads(api_response.text)
+            print "whatever this is: " + str(response_json['data'])
+            response_data = transform_motifs_to_transcription_factors(response_json['data'])
+            status_message = 'Got ' + str(response_json['hitcount']) + ' rows back from API.'
+            status_message+= ' page shown '+str(search_request_params['page_of_results_to_display'])
+            form_data['page_of_results_shown'] = search_request_params['page_of_results_to_display']
+            search_paging_info = get_paging_info_for_display(response_json['hitcount'],
+                                             search_request_params['page_of_results_to_display'])
+        new_form = SearchByGenomicLocationForm(form_data)
+        return render(request, searchpage_template, 
+                                        { 'api_response' : response_data,
+                                          'tf_search_form' : SearchByTranscriptionFactorForm(),
+                                          'gl_search_form': new_form, 
+                                          'snpid_search_form' : SearchBySnpidForm(),
+                                          'holdover_gl_region': api_search_query,
+                                          'search_paging_info' : search_paging_info,
+                                          'active_tab'     : 'gl-region',
+                                          'status_message' : status_message})                                              
 
 
-# translating from TFs to motifs is handled in the setup for
-# form choices.....
-# ...  or.. , it would be if it was a 1-to-1 mapping back onto MOTIFs
 def lookup_motif_by_tf(trans_factor):
     lut = None
     #TODO: the following pickle must be processed in such a way that a 
