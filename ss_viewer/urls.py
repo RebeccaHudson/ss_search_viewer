5e884c47fda499fece3369eb66d68f064fc8ab2a
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-06-16 23:59:42 +0000

    Add exactly one line of whitespace between each URL.

--- ss_viewer/urls.py
+++ ss_viewer/urls.py
@@ -7,9 +7,12 @@ app_name = 'ss_viewer'
 
 urlpatterns = [
   url(r'^$', views.main.index, name = 'index'),
-  url(r'one-snp-detail/rs(?P<snpid_numeric>[0-9]+)/$', views.main.one_snp_detail, name = 'one-snp-detail'),  #remove this shit 
+
   url(r'^multi-search/$', views.main.show_multisearch_page, name='multi-search'),
+
   url(r'^gl-region-search/$', views.main.handle_search_by_genomic_location, name='gl-region-search'),
+
   url(r'^trans-factor-search/$', views.main.handle_search_by_trans_factor, name='trans-factor-search'),
+
   url(r'snpid-search/$', views.main.handle_search_by_snpid, name = 'snpid-search'),
 ]
