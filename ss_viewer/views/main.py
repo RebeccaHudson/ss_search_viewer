cc46fa3ccbafbbb5ece1933cb74900992a5e2016
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-06-17 00:21:45 +0000

    Remove a whole bunch of commented-out code.

--- ss_viewer/views/main.py
+++ ss_viewer/views/main.py
@@ -33,68 +33,27 @@ def index(request):
 
 def setup_api_url(api_function, snpid=None):
   return APIUrls.setup_api_url(api_function)
-  #can probably just remove all of this. 
-  #hostinfo = settings.API_HOST_INFO 
-  #host_w_port = ':'.join([ hostinfo['host_url'], hostinfo['host_port'] ] )
-  #url_arglist =  [ hostinfo['api_root'], api_function] 
-  #if snpid:
-  #  url_arglist.append(snpid)
-  #url_args = '/'.join(url_arglist) 
-  #url = host_w_port + "/" + url_args  + "/"
-  #return url
 
 #this is shared, I think.
 def transform_motifs_to_transcription_factors(response_json):
     tft = MotifTransformer()
     return tft.transform_motifs_to_transcription_factors(response_json)
-    #moved into shared in related class:
-    #transformed_response = []
-    #
-    #for one_row in response_json:
-    #    motif_value = one_row['motif']
-    #    one_row['trans_factor'] = tft.transform_one_motif_to_trans_factor(motif_value)
-    #    transformed_response.append(one_row) 
-
-    #return transformed_response 
-
 
 def extract_snpids_from_textfield(text):
   return SnpidSearchUtils.extract_snpids_from_textfield(text) 
-  #gex = re.compile('(rs[0-9]+)', re.MULTILINE)  
-  #list_of_snpids = gex.findall(text)
-  #return list_of_snpids
 
 # TODO refer to a default p-value from settings.
 def get_pvalue_rank_from_form(form):
   return PValueFromForm.get_pvalue_rank_from_form(form)
-  #if form.cleaned_data.has_key('pvalue_rank_cutoff'):
-  #  return form.cleaned_data.get('pvalue_rank_cutoff')
-  #else:
-  #  return 0.05 
    
 #this should go into snpid search stuff.
 def clean_and_validate_snpid_text_input(text_input):
     return SnpidSearchUtils.clean_and_validate_snpid_text_input(text_input)
-#  snpids = extract_snpids_from_textfield(text_input)
-#  deduped_snpids = list(set(snpids))  #don't allow any duplicate requests.
-#  if len(deduped_snpids) == 0:  
-#    raise forms.ValidationError("No snpids have been included")  
-#  return deduped_snpids
-
 
 # Either by textarea or by file, snpid search form is not 
 # valid unless one of these is present 
 def get_snpid_list_from_form(request, form):
     return SnpidSearchUtils.get_snpid_list_from_form(request, form)
-    #  form_snpids = form.cleaned_data.get('raw_requested_snpids')
-    #  #remove the data in there.
-    #  if form_snpids:
-    #    return clean_and_validate_snpid_text_input(form_snpids)
-    #  else:
-    #    file_pointer = request.FILES.get('file_of_snpids')
-    #    text_in_file = file_pointer.read()   # TODO: read in chunks rather than all at once. 
-    #    return clean_and_validate_snpid_text_input(text_in_file) 
-    #
 
 def setup_context_for_snpid_search_results(api_response, snpid_list, holdover_p_value, 
                                            page_of_results_to_display):
@@ -120,75 +79,8 @@ def setup_context_for_snpid_search_results(api_response, snpid_list, holdover_p_
                     'search_paging_info' : search_paging_info }) 
     return context
 
-#this should go into shared.
-#DRY up the boilerplate.
 def setup_formset_context(tf_form=None, gl_form=None, snpid_form=None):
     return StandardFormset.setup_formset_context(tf_form, gl_form, snpid_form)
-    # 
-    #active_tab = None
-    #hidden_page_number = {'page_of_results_shown':0}
-    #if tf_form is None:
-    #    tf_form = SearchByTranscriptionFactorForm(initial=hidden_page_number)
-    #else:
-    #    active_tab = 'tf'
-    #if gl_form is None:
-    #    gl_form = SearchByGenomicLocationForm(initial=hidden_page_number)
-    #else: 
-    #    active_tab = 'gl-region'
-    #if snpid_form is None:
-    #    snpid_form = SearchBySnpidForm(initial=hidden_page_number)
-    #else:
-    #    active_tab = 'snpid'
-    #context =  { 'tf_search_form' : tf_form,
-    #            'gl_search_form': gl_form, 
-    #            'snpid_search_form' : snpid_form }                                              
-    #if active_tab is not None:
-    #    context.update({'active_tab': active_tab })
-    #return context
-
-def handle_search_by_snpid(request):
-  if request.method == 'GET':
-      return redirect(reverse('ss_viewer:multi-search'))
-
-  if request.method == 'POST':
-    searchpage_template = 'ss_viewer/multi-searchpage.html'  
-    snpid_search_form = SearchBySnpidForm(request.POST, request.FILES)
-    status_message = ""    
-    if not snpid_search_form.is_valid():
-       context = setup_formset_context(snpid_form=snpid_search_form)
-       context.update({'status_message':'Invalid search. Try agian.'})
-       return render(request, searchpage_template, context)
-    #if execution reaches this point, the form is valid.
-    snpid_list = None
-    try:
-        snpid_list = get_snpid_list_from_form(request, snpid_search_form)
-
-    except ValidationError:
-        context = setup_formset_context()
-        context.update({'status_message' : "No properly formatted SNPids in the text.",
-                        'active_tab'     : 'snpid' })
-        return render(request, searchpage_template, context)
-
-    form_data = snpid_search_form.cleaned_data
-    search_request_params = get_paging_info_for_request(request, 
-                                                form_data['page_of_results_shown'])
-
-    pvalue_rank = get_pvalue_rank_from_form(snpid_search_form)
-    request_data = { 'snpid_list' : snpid_list, 
-                     'pvalue_rank' : pvalue_rank,
-                     'from_result' : search_request_params['search_result_offset'] }
- 
-    api_response = requests.post( setup_api_url('snpid-search'), 
-                                  json=request_data, 
-                                  headers={ 'content-type' : 'application/json' })
-
-    context = setup_context_for_snpid_search_results(api_response, snpid_list, 
-                       pvalue_rank ,search_request_params['page_of_results_to_display'])  
-    return render(request, searchpage_template, context )
-
-
-
-
 
 #The above function 'get_scores_for_list' should actually be called
 #handle search by genomic location.. 
