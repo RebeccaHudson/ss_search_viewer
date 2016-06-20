de89605d75a124aecd153d7c5fef3f07bf84cedd
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-06-20 00:09:29 +0000

    The table is now actually OK looking, and gene name search is ready to return no data at all if you put an empty search into it.

--- ss_viewer/views/gene_name_search.py
+++ ss_viewer/views/gene_name_search.py
@@ -21,27 +21,27 @@ def handle_gene_name_search(request):
 
     if not gene_name_search_form.is_valid():
         context = StandardFormset.setup_formset_context(
-                                     gene_name_form = gene_name_form)
+                                     gene_name_form = gene_name_search_form)
         return StandardFormset.handle_invalid_form(request, context)
 
-    form_data = snpid_window_search_form.cleaned_data
+    form_data = gene_name_search_form.cleaned_data
     search_request_params = Paging.get_paging_info_for_request(request,
                                                 form_data['page_of_results_shown'])
 
-    pvalue_rank = PValueFromForm.get_pvalue_rank_from_form(snpid_window_search_form)
+    pvalue_rank = PValueFromForm.get_pvalue_rank_from_form(gene_name_search_form)
     api_search_query = {'gene_name' : form_data['gene_name'], 
                         'window_size' : form_data['window_size'],
                         'pvalue_rank':   pvalue_rank,
                         'from_result' : search_request_params['search_result_offset']}
     shared_context = APIResponseHandler.handle_search(api_search_query, 
-                                                      'search-by-window-around-snpid',
+                                                      'search-by-gene-name',
                                                       search_request_params)
     #the next line of code 'turns the page'
     form_data['page_of_results_shown'] = search_request_params['page_of_results_to_display']
-    snpid_window_search_form = SearchByGeneNameForm(form_data)
+    gene_name_search_form = SearchByGeneNameForm(form_data)
 
     context = StandardFormset.setup_formset_context(
-                                           gene_name_form=snpid_window_search_form)
+                                           gene_name_form=gene_name_search_form)
     context.update(shared_context)
     return render(request, 
                  'ss_viewer/multi-searchpage.html',
