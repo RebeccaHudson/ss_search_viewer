e9551e4ee57aea0493e77730f382312ccd70a21d
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-06-23 00:19:35 +0000

    Add support for downloading results to searches by snpid window.

--- ss_viewer/views/snpid_window_search.py
+++ ss_viewer/views/snpid_window_search.py
@@ -25,15 +25,24 @@ def handle_snpid_window_search(request):
         return StandardFormset.handle_invalid_form(request, context)
 
     form_data = snpid_window_search_form.cleaned_data
-    search_request_params = Paging.get_paging_info_for_request(request,
-                                                form_data['page_of_results_shown'])
+
     requested_snpid = form_data['snpid']
     window_size = form_data['window_size']
     pvalue_rank = PValueFromForm.get_pvalue_rank_from_form(snpid_window_search_form)
-    api_search_query = {'snpid' : requested_snpid, 
-                        'window_size' : window_size,
-                        'pvalue_rank':   pvalue_rank,
-                        'from_result' : search_request_params['search_result_offset']}
+
+    base_search_params = {'snpid' : requested_snpid, 
+                          'window_size' : window_size,
+                          'pvalue_rank':   pvalue_rank}
+      
+    if request.POST['action'] == 'Download Results':
+        return APIResponseHandler.handle_download_request(base_search_params,
+                                                 'search-by-window-around-snpid')
+
+    search_request_params = Paging.get_paging_info_for_request(request,
+                                                form_data['page_of_results_shown'])
+
+    api_search_query = base_search_params
+    api_search_query.update({'from_result' : search_request_params['search_result_offset']})
     shared_context = APIResponseHandler.handle_search(api_search_query, 
                                                       'search-by-window-around-snpid',
                                                       search_request_params)
