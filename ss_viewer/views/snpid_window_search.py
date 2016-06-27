2e20fb98ae24d9a20ecc5afbe7cada44da953a21
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-06-27 02:50:05 +0000

    Make larger-size downloads work properly for snpid window searches.

--- ss_viewer/views/snpid_window_search.py
+++ ss_viewer/views/snpid_window_search.py
@@ -12,6 +12,14 @@ from ss_viewer.views.shared import APIUrls
 from ss_viewer.views.shared import StandardFormset 
 from ss_viewer.views.shared import APIResponseHandler 
 
+from ss_viewer.views.shared import StreamingCSVDownloadHandler 
+
+def copy_valid_form_data_into_hidden_fields(form_data):
+    fields_to_copy = ['pvalue_rank_cutoff', 'snpid', 'window_size']
+    for form_field in fields_to_copy:
+        form_data['prev_search_' + form_field] = form_data[form_field]
+    return form_data
+
 
 def handle_snpid_window_search(request):
     if request.method != 'POST':
@@ -30,23 +38,31 @@ def handle_snpid_window_search(request):
     window_size = form_data['window_size']
     pvalue_rank = PValueFromForm.get_pvalue_rank_from_form(snpid_window_search_form)
 
-    base_search_params = {'snpid' : requested_snpid, 
-                          'window_size' : window_size,
-                          'pvalue_rank':   pvalue_rank}
+
       
     if request.POST['action'] == 'Download Results':
-        return APIResponseHandler.handle_download_request(base_search_params,
-                                                 'search-by-window-around-snpid')
+        pavlue_rank = form_data['prev_search_pvalue_rank_cutoff']
+        window_size = form_data['prev_search_window_size']
+        snpid = form_data['prev_search_snpid']
+        previous_search_params = { 'snpid'       : snpid, 
+                                   'window_size' : window_size, 
+                                   'pvalue_rank' : pvalue_rank  } 
+        return StreamingCSVDownloadHandler.streaming_csv_view(request, 
+                                                              previous_search_params, 
+                                                              'search-by-window-around-snpid')
 
     search_request_params = Paging.get_paging_info_for_request(request,
                                                 form_data['page_of_results_shown'])
 
-    api_search_query = base_search_params
-    api_search_query.update({'from_result' : search_request_params['search_result_offset']})
+    api_search_query =  {'snpid'       : requested_snpid, 
+                         'window_size' : window_size,
+                         'pvalue_rank' :   pvalue_rank,
+                         'from_result' : search_request_params['search_result_offset']}
     shared_context = APIResponseHandler.handle_search(api_search_query, 
                                                       'search-by-window-around-snpid',
                                                       search_request_params)
     #the next line of code 'turns the page'
+    form_data = copy_valid_form_data_into_hidden_fields(form_data)
     form_data['page_of_results_shown'] = search_request_params['page_of_results_to_display']
     snpid_window_search_form = SearchBySnpidWindowForm(form_data)
     context = StandardFormset.setup_formset_context(
