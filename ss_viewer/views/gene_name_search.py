a6cb04c794e4f0942f082a440efdb0b506339e77
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-06-27 03:40:19 +0000

    Finally, add support for larger downloads to searches by gene name.

--- ss_viewer/views/gene_name_search.py
+++ ss_viewer/views/gene_name_search.py
@@ -11,6 +11,13 @@ from ss_viewer.views.shared import MotifTransformer, TFTransformer
 from ss_viewer.views.shared import APIUrls 
 from ss_viewer.views.shared import StandardFormset 
 from ss_viewer.views.shared import APIResponseHandler 
+from ss_viewer.views.shared import StreamingCSVDownloadHandler
+
+def copy_valid_form_data_into_hidden_fields(form_data):
+    fields_to_copy = ['gene_name', 'window_size', 'pvalue_rank_cutoff']
+    for form_field in fields_to_copy:
+        form_data['prev_search_'+form_field] = form_data[form_field]
+    return form_data
 
 
 def handle_gene_name_search(request):
@@ -26,21 +33,29 @@ def handle_gene_name_search(request):
 
     form_data = gene_name_search_form.cleaned_data
     pvalue_rank = PValueFromForm.get_pvalue_rank_from_form(gene_name_search_form)
-    base_search_params = {'gene_name' : form_data['gene_name'], 
-                          'window_size' : form_data['window_size'],
-                          'pvalue_rank':   pvalue_rank }
-    if request.POST['action'] == 'Download Results':
-        return APIResponseHandler.handle_download_request(
-                                     base_search_params, 'search-by-gene-name')
 
+    if request.POST['action'] == 'Download Results':
+        gene_name = form_data['prev_search_gene_name']
+        pvalue_rank = form_data['prev_search_pvalue_rank_cutoff']
+        window_size = form_data['prev_search_window_size']
+        previous_search_params = { 'gene_name'   :  gene_name,
+                                   'pvalue_rank' :  pvalue_rank,
+                                   'window_size' :  window_size  }
+        return StreamingCSVDownloadHandler.streaming_csv_view(request,
+                                                              previous_search_params, 
+                                                              'search-by-gene-name')
     search_request_params = Paging.get_paging_info_for_request(request,
                                                 form_data['page_of_results_shown'])
-    api_search_query = base_search_params
-    api_search_query.update({'from_result' : search_request_params['search_result_offset']})
+    api_search_query  =  {'gene_name'   : form_data['gene_name'], 
+                          'window_size' : form_data['window_size'],
+                          'pvalue_rank' :  pvalue_rank, 
+                          'from_result' : search_request_params['search_result_offset']}
+
     shared_context = APIResponseHandler.handle_search(api_search_query, 
                                                       'search-by-gene-name',
                                                       search_request_params)
     #the next line of code 'turns the page'
+    form_data = copy_valid_form_data_into_hidden_fields(form_data)
     form_data['page_of_results_shown'] = search_request_params['page_of_results_to_display']
     gene_name_search_form = SearchByGeneNameForm(form_data)
 
