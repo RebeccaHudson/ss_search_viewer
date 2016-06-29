8a3f3a146670f6c1090c91bf599fe97f5c7e85fd
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-06-29 00:16:29 +0000

    Make searches by genomic location not blow up in the user's face if the start position is missing.

--- ss_viewer/views/gl_search.py
+++ ss_viewer/views/gl_search.py
@@ -19,17 +19,36 @@ def copy_valid_form_data_into_hidden_fields(valid_form_data):
         valid_form_data['prev_search_'+form_field] = valid_form_data[form_field]
     return valid_form_data
 
+
+#can't make django do what it's supposed to w/ form validation; so check fields here..
+def field_checker(request, form_data):
+   form_msg = ""
+   if form_data.get('selected_chromosome') is None:
+       form_msg += "Missing chromosome value."
+   if form_data.get('gl_start_pos') is None:
+       form_msg += " Missing start position. (required)"
+   if form_data.get('gl_end_pos') is None:
+       form_msg += " Missing end position."
+   if form_msg == "":
+       return None
+   return form_msg
+
+
 def handle_search_by_genomic_location(request):
     if request.method != 'POST':
         return redirect(reverse('ss_viewer:multi-search'))
 
     gl_search_form = SearchByGenomicLocationForm(request.POST)
+ 
     if not gl_search_form.is_valid() and not request.POST['action'] != 'Download Results':
          context = StandardFormset.setup_formset_context(gl_form=gl_search_form)
          return StandardFormset.handle_invalid_form(request, context)
-    
+   
+    print "about to get cleaned data"  
     form_data = gl_search_form.cleaned_data
-    
+    print "just got cleaned form data " + str(form_data) 
+
+
     #download is offered for results currently displayed.
     if request.POST['action'] == 'Download Results':
         previous_search_params = { 'chromosome' : form_data['prev_search_selected_chromosome'],
@@ -42,8 +61,17 @@ def handle_search_by_genomic_location(request):
                                                              'search-by-gl')
 
     pvalue = PValueFromForm.get_pvalue_rank_from_form(gl_search_form)
+    if form_data['pvalue_rank_cutoff'] is None:
+       form_data['pvalue_rank_cutoff'] = pvalue
+
     search_request_params = Paging.get_paging_info_for_request(request,
                                                          form_data['page_of_results_shown'])
+    possible_errors = field_checker(request, form_data)
+
+    if possible_errors is not None:
+         context = StandardFormset.setup_formset_context(gl_form=gl_search_form)
+         return StandardFormset.handle_invalid_form(request, context, status_message=possible_errors)
+
     api_search_query =  { 'chromosome' : form_data['selected_chromosome'],
                           'start_pos'  : form_data['gl_start_pos'],
                           'end_pos'    : form_data['gl_end_pos'],
