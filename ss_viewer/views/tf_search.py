d6cf3985d0aa083cebb890cfd68d54e3a29eed01
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2016-06-29 00:18:08 +0000

    Fix searches by transcription factor to handle either ENCODE or JASPAR motifs.

--- ss_viewer/views/tf_search.py
+++ ss_viewer/views/tf_search.py
@@ -14,7 +14,7 @@ from ss_viewer.views.shared import APIResponseHandler
 from ss_viewer.views.shared import StreamingCSVDownloadHandler 
 
 def copy_valid_form_data_into_hidden_fields(form_data):
-    fields_to_copy = ['pvalue_rank_cutoff', 'trans_factor']
+    fields_to_copy = ['pvalue_rank_cutoff', 'trans_factor', 'encode_trans_factor', 'tf_library']
     for form_field in fields_to_copy:
         form_data['prev_search_'+form_field] = form_data[form_field]
     return form_data
@@ -32,31 +32,81 @@ def handle_search_by_trans_factor(request):
 
     form_data = tf_search_form.cleaned_data
 
+    pvalue_rank = PValueFromForm.get_pvalue_rank_from_form(tf_search_form)
+
+    print "form data for tf search " + str(form_data)
+    # ENCODE motifs are prefixes, JASPAR are actually mapped with TFTransformer
+    if form_data['tf_library'] == 'encode':
+        return handle_search_by_encode_trans_factor(request, form_data, pvalue_rank)   #this logic is inelegant because I need to 
+                                                                   # finish it ASAP at this point.; consider tidying up.
+    #otherwise, go with the previously-developed JASPAR library behavior (lookups for transcription factors)
+    print "searching by jaspar.."
+
     tft = TFTransformer()
     #offer a download of results currently shown, use the values copied into the 
     #hidden controls on the previous form POST.
     if request.POST['action'] == 'Download Results':
         motif_value = tft.lookup_motifs_by_tf(form_data['prev_search_trans_factor'])
         pvalue_rank = form_data['prev_search_pvalue_rank_cutoff']
-        previous_search_params = {'motif' : motif_value, 'pvalue_rank':   pvalue_rank}
+        previous_search_params = {'motif'  :  motif_value, 
+                             'pvalue_rank' :  pvalue_rank,
+                              'tf_library' : 'jaspar' }
         return StreamingCSVDownloadHandler.streaming_csv_view(request, 
                                                               previous_search_params, 
                                                               'search-by-tf')
 
 
     motif_value = tft.lookup_motifs_by_tf(form_data['trans_factor'])
-
-    pvalue_rank = PValueFromForm.get_pvalue_rank_from_form(tf_search_form)
-
     search_request_params = Paging.get_paging_info_for_request(request,
                                                 form_data['page_of_results_shown'])
-
-    api_search_query = {'motif' : motif_value, 'pvalue_rank':   pvalue_rank}
-    api_search_query.update({'from_result' : search_request_params['search_result_offset']})
+    api_search_query = {'motif' : motif_value, 
+                        'pvalue_rank':   pvalue_rank,
+                        'from_result' : search_request_params['search_result_offset']}
 
     shared_context = APIResponseHandler.handle_search(api_search_query, 
                                                       'search-by-tf',
                                                       search_request_params)
+    form_data = copy_valid_form_data_into_hidden_fields(form_data) 
+    #the next line of code 'turns the page'
+    form_data['page_of_results_shown'] = search_request_params['page_of_results_to_display']
+
+    tf_search_form = SearchByTranscriptionFactorForm(form_data)
+    context = StandardFormset.setup_formset_context(tf_form=tf_search_form)
+    context.update(shared_context)
+
+    return render(request, 
+                 'ss_viewer/multi-searchpage.html',
+                  context)
+
+
+
+
+# Note/remember for ENCODE, the transcription factor is ABC the motif value is ABC-omg-why-is-this-name-so-long
+def  handle_search_by_encode_trans_factor(request, form_data, pvalue_rank): 
+    print "Searching by encode"
+    if request.POST['action'] == 'Download Results':
+        motif_prefix = form_data['prev_search_encode_trans_factor']
+        pvalue_rank = form_data['prev_search_pvalue_rank_cutoff']
+        previous_search_params = {'tf_library' :  'encode',
+                                  'motif'      :  motif_prefix,
+                                  'pvalue_rank':  pvalue_rank}
+        return StreamingCSVDownloadHandler.streaming_csv_view(request, 
+                                                              previous_search_params, 
+                                                              'search-by-tf')
+
+    motif_prefix = form_data['encode_trans_factor']
+    #pvalue_rank = PValueFromForm.get_pvalue_rank_from_form(tf_search_form)
+    search_request_params = Paging.get_paging_info_for_request(request,
+                                                form_data['page_of_results_shown'])
+    api_search_query = {'tf_library'  :  'encode', 
+                        'motif'       :  motif_prefix, 
+                        'pvalue_rank' :  pvalue_rank,
+                        'from_result' :  search_request_params['search_result_offset']}
+
+    shared_context = APIResponseHandler.handle_search(api_search_query, 
+                                                      'search-by-tf',
+                                                      search_request_params)
+
     form_data = copy_valid_form_data_into_hidden_fields(form_data) 
     #the next line of code 'turns the page'
     form_data['page_of_results_shown'] = search_request_params['page_of_results_to_display']
