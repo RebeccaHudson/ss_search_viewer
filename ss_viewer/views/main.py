f44af0c6a8ef1dce289c23bb749f6ddf44b6543a
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2017-01-31 02:28:11 +0000

    Comment out code that's probably unused permanently to grab SVG plot sources from Elasticsearch.

--- ss_viewer/views/main.py
+++ ss_viewer/views/main.py
@@ -16,7 +16,6 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from ss_viewer.views.shared import MotifTransformer
 from ss_viewer.views.shared import TFTransformer
 from ss_viewer.views.shared import APIUrls 
-from ss_viewer.views.shared import PValueFromForm 
 from ss_viewer.views.shared import StandardFormset
 
 
@@ -34,55 +33,55 @@ import zipfile
 def index(request):
   return HttpResponse("Try another url, like :ss_viewer/multi-search.")
 
-def find_working_es_url():
-    found_working = False
-    i = 0 
-    #db-1 is broken for some reason; normally would start at 0
-    while found_working is False:
-        url_to_try = settings.ELASTICSEARCH_URLS[i] + '/atsnp_data/atsnp_output/_search?size=1'
-        print "trying this url " + url_to_try
-        es_check_response = None
-        try:
-            es_check_response = requests.get(url_to_try, timeout=35)  
-        except requests.exceptions.Timeout:
-            print "request for search at : " + url_to_try +  " timed out."  
-        else:    
-            print "url " + url_to_try + " es_check_response" + str(json.loads(es_check_response.text))
-            es_check_data = json.loads(es_check_response.text)
-            return settings.ELASTICSEARCH_URLS[i]
-        i += 1
-        if i > 2:  
-            return None
-
-def get_plot_data_out_of_es(plot_info):
-    #es_url = 'http://atsnp-db1.biostat.wisc.edu:9200'          
-    es_url = find_working_es_url()
-    es_index = '/atsnp_data'
-    es_type = '/svg_plots'
-    search_endpoint = '/_search?size=1'
-    plot_search_url = es_url + es_index + es_type + search_endpoint 
-    q = { "query" : {
-        "bool" : {
-          "must" : [ 
-                   { "match" : { "snpid"      : plot_info['snpid']     } },
-                   { "match" : { "motif"      : plot_info['motif']     } },
-                   { "match" : { "snp_allele" : plot_info['snp_allele'] } },
-                 ]     } } }
-    results = requests.post(plot_search_url, data=json.dumps(q))
-    if results.status_code == 200: 
-        #svg_data = results.json()['hits']['hits'][0]['svg_plot']    
-        result_json = results.json()
-        hits = result_json['hits']['hits']
-        print "hits : " + str(len(hits))
-        if len(hits) > 0:
-            print "data keys  " + str(hits[0].keys())
-            doc_source = hits[0]['_source']
-            svg_data = doc_source['svg_plot']
-            #return "nothing special"
-            return svg_data 
-        print result_json
-    print "no plot found for " + str(plot_info)
-    return None
+#def find_working_es_url():
+#    found_working = False
+#    i = 0 
+#    #db-1 is broken for some reason; normally would start at 0
+#    while found_working is False:
+#        url_to_try = settings.ELASTICSEARCH_URLS[i] + '/atsnp_data/atsnp_output/_search?size=1'
+#        print "trying this url " + url_to_try
+#        es_check_response = None
+#        try:
+#            es_check_response = requests.get(url_to_try, timeout=35)  
+#        except requests.exceptions.Timeout:
+#            print "request for search at : " + url_to_try +  " timed out."  
+#        else:    
+#            print "url " + url_to_try + " es_check_response" + str(json.loads(es_check_response.text))
+#            es_check_data = json.loads(es_check_response.text)
+#            return settings.ELASTICSEARCH_URLS[i]
+#        i += 1
+#        if i > 2:  
+#            return None
+#
+#def get_plot_data_out_of_es(plot_info):
+#    #es_url = 'http://atsnp-db1.biostat.wisc.edu:9200'          
+#    es_url = find_working_es_url()
+#    es_index = '/atsnp_data'
+#    es_type = '/svg_plots'
+#    search_endpoint = '/_search?size=1'
+#    plot_search_url = es_url + es_index + es_type + search_endpoint 
+#    q = { "query" : {
+#        "bool" : {
+#          "must" : [ 
+#                   { "match" : { "snpid"      : plot_info['snpid']     } },
+#                   { "match" : { "motif"      : plot_info['motif']     } },
+#                   { "match" : { "snp_allele" : plot_info['snp_allele'] } },
+#                 ]     } } }
+#    results = requests.post(plot_search_url, data=json.dumps(q))
+#    if results.status_code == 200: 
+#        #svg_data = results.json()['hits']['hits'][0]['svg_plot']    
+#        result_json = results.json()
+#        hits = result_json['hits']['hits']
+#        print "hits : " + str(len(hits))
+#        if len(hits) > 0:
+#            print "data keys  " + str(hits[0].keys())
+#            doc_source = hits[0]['_source']
+#            svg_data = doc_source['svg_plot']
+#            #return "nothing special"
+#            return svg_data 
+#        print result_json
+#    print "no plot found for " + str(plot_info)
+#    return None
 
 def get_plot_query_info_from_string(query_str):
     split_parts = query_str.split("_")
@@ -139,28 +138,28 @@ def test_svg_plots(request):
 
 
 
-def get_a_plot_by_snpid_and_motif(snpid, motif):  #TODO: should take a snpid!
-    #path_to_images = os.path.join(os.path.dirname(__file__), 'pictures')
-    #path_to_image = os.path.join(path_to_images, 'test_plot.svg')
-    url = setup_api_url('plotting-data')
-    headers = { 'content-type':'application/json' }
-    api_search_query = { 'snpid': snpid, 'motif': motif }
-    api_response = requests.post(url, 
-                                 json=api_search_query,
-                                 headers=headers)
-    response_json = None
-    if api_response.status_code == 204:
-        plot_status_message = 'No matching rows.'
-    else:
-        response_json = json.loads(api_response.text)
-        status_message = 'Got ' + str(len(response_json)) + ' rows back from API.'
-        print("here's the API response" + str(response_json))
-
-        plotter = MakePlots(response_json)
-        plotter.make_plot()
-
-    path_to_image = os.path.join("pictures", 'test_plot.svg')
-    return { 'image_path' : path_to_image, 'plotting_data' : response_json }
+#def get_a_plot_by_snpid_and_motif(snpid, motif):  #TODO: should take a snpid!
+#    #path_to_images = os.path.join(os.path.dirname(__file__), 'pictures')
+#    #path_to_image = os.path.join(path_to_images, 'test_plot.svg')
+#    url = setup_api_url('plotting-data')
+#    headers = { 'content-type':'application/json' }
+#    api_search_query = { 'snpid': snpid, 'motif': motif }
+#    api_response = requests.post(url, 
+#                                 json=api_search_query,
+#                                 headers=headers)
+#    response_json = None
+#    if api_response.status_code == 204:
+#        plot_status_message = 'No matching rows.'
+#    else:
+#        response_json = json.loads(api_response.text)
+#        status_message = 'Got ' + str(len(response_json)) + ' rows back from API.'
+#        print("here's the API response" + str(response_json))
+#
+#        plotter = MakePlots(response_json)
+#        plotter.make_plot()
+#
+#    path_to_image = os.path.join("pictures", 'test_plot.svg')
+#    return { 'image_path' : path_to_image, 'plotting_data' : response_json }
 
 
 def help_page(request):
