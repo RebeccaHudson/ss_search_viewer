1c0226c9e6500009bfb17985bedd4004fb24a6e1
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2017-01-31 02:33:42 +0000

    Remove a bunch of commented-out URL routes from refactoring.

--- ss_viewer/urls.py
+++ ss_viewer/urls.py
@@ -15,37 +15,10 @@ from .views.snpid_window_search import SnpidWindowSearchView
 from .views.snpid_search import SnpidSearchView
 app_name = 'ss_viewer'
 
-
- #  url(r'^$',
- #      views.shared.StandardFormset.show_multisearch_page,
- #      name = 'index'),
- # For home page to be the search page.
-
-#known-working version:
-#  url(r'^trans-factor-search/$',
-#      views.tf_search.handle_search_by_trans_factor,
-#      name='trans-factor-search'),
-
-#  url(r'^gl-region-search/$', 
-#      views.gl_search.handle_search_by_genomic_location,
-#      name='gl-region-search'),
-
-#  url(r'gene-name-search/$', 
-#       views.gene_name_search.handle_gene_name_search,
-#       name='gene-name-search')
-
-#  url(r'snpid-window-search/$',
-#       views.snpid_window_search.handle_snpid_window_search,
-#       name='snpid-window-search'),
-
-#  url(r'snpid-search/$', 
-#     views.snpid_search.handle_search_by_snpid, 
-#     name='snpid-search'),
-
 urlpatterns = [
   url(r'^$', 
          views.main.home_page,
-         name = 'index'),           #does this need also to be 'home-page'?
+         name = 'index'), 
 
   url (r'^dynamic-svg/(?P<plot_id_string>.+)$',
        views.main.dynamic_svg,
