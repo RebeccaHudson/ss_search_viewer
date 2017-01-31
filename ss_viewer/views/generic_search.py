e181e0ea812018e171749ea15bed27455319d771
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2017-01-31 02:43:36 +0000

    Clean up unneeded comments and tighten up spacing.

--- ss_viewer/views/generic_search.py
+++ ss_viewer/views/generic_search.py
@@ -7,28 +7,17 @@ from django.shortcuts import redirect
 from django.views.generic import View
 from ss_viewer.views.shared import StandardFormset
 from ss_viewer.views.shared import PValueDictFromForm
-
 from ss_viewer.views.shared import Paging
-
-
 from ss_viewer.views.shared import StreamingCSVDownloadHandler
 
 class GenericSearchView(View):
-    #form_class = MyForm #this needs to be parametrized/overridden
-                        #should be overridden in each inheriting class. 
-    #initial = {'key': 'value'}
     template_name = 'ss_viewer/multi-searchpage.html'
-    search_form = None #this means it's available, right?
+    search_form = None 
 
     #For search views, this should just point back to the main search page.
     def get(self, request, *args, **kwargs):
-        #form = self.form_class()
-        #TODO: verify that 'form' is what the form is called in context.
         return redirect(reverse('ss_viewer:multi-search')) 
-
    
-    #put code that is shared in here... 
-    #what can be 'super()-ed?'
     def post(self, request, *args, **kwargs):
         search_form = None 
         if request.POST['action'] in ['Prev', 'Next']:
@@ -54,7 +43,6 @@ class GenericSearchView(View):
         #EG: would I just call 'data' or something?
         if request.POST['action'] == 'Download Results':
             return self.handle_download(self.search_form.cleaned_data, request)
-
       
         search_request_params = Paging.get_paging_info_for_request(request, 
                                              form_data['page_of_results_shown']) 
@@ -71,8 +59,6 @@ class GenericSearchView(View):
                                                  search_request_params)
         context.update(shared_context)
         return render(request, 'ss_viewer/multi-searchpage.html',  context)
-
-
          
     def handle_paging_and_return_context(self, form_data,
                                                     search_request_params):
@@ -83,8 +69,6 @@ class GenericSearchView(View):
         context = StandardFormset.dict_based_setup_formset_context(
                                    {self.form_name_in_context:self.search_form})
         return context
-
-
 
     def handle_download(self, form_data, request):
         prev_search_params = self.handle_params_for_download(form_data)
@@ -97,8 +81,6 @@ class GenericSearchView(View):
         return StreamingCSVDownloadHandler.streaming_csv_view(request, 
                                                                prev_search_params, 
                                                                self.api_action_name)
- 
-
     def get_pvalues_from_form(self):
         pvalues_for_search = {}
         pvalue_dict = PValueDictFromForm.get_pvalues_from_form(self.search_form) 
@@ -126,22 +108,3 @@ class GenericSearchView(View):
             if 'prev_search_' + form_field in form_data:
                 form_data[form_field]  = form_data['prev_search_' + form_field]
         return form_data
-
-#functional-based view.
-#from django.http import HttpResponseRedirect
-#from django.shortcuts import render
-#
-#from .forms import MyForm
-#
-#def (request):
-#    if request.method == "POST":
-#        form = MyForm(request.POST)
-#        if form.is_valid():
-#            # <process form cleaned data>
-#            return HttpResponseRedirect('/success/')
-#    else:
-#        form = MyForm(initial={'key': 'value'})
-#
-#    return render(request, 'form_template.html', {'form': form})
-
-
