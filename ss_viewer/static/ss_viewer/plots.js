8786fafa81996351bea99df5b62c3374f0c6980b
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2017-03-02 00:28:29 +0000

    Unindent.

--- ss_viewer/static/ss_viewer/plots.js
+++ ss_viewer/static/ss_viewer/plots.js
@@ -1,19 +1,19 @@
-    //Draws plots in-line inside the table of search results.
-    function drawPlotsInline(){
-       /* plotting data from each of the searched items.*/
-       //moved into a function for calling when AJAX results are returned.
-       var plottingData = [];
-       $("td.plotting_data" ).each(function() {
-           var data_for_one_plot = this.textContent;
-           onePlotData = jQuery.parseJSON(data_for_one_plot);
-           plottingData.push(onePlotData);
-           //console.log("preparing for one plot.. " + data_for_one_plot ); 
-       });
-       cloneSVGSkeleton(plottingData);
-       $("svg#target-0").hide();
-       $("svg#target-refhalf-0").hide();
-       $("svg#target-snphalf-0").hide();
-    }
+//Draws plots in-line inside the table of search results.
+function drawPlotsInline(){
+   /* plotting data from each of the searched items.*/
+   //moved into a function for calling when AJAX results are returned.
+   var plottingData = [];
+   $("td.plotting_data" ).each(function() {
+       var data_for_one_plot = this.textContent;
+       onePlotData = jQuery.parseJSON(data_for_one_plot);
+       plottingData.push(onePlotData);
+       //console.log("preparing for one plot.. " + data_for_one_plot ); 
+   });
+   cloneSVGSkeleton(plottingData);
+   $("svg#target-0").hide();
+   $("svg#target-refhalf-0").hide();
+   $("svg#target-snphalf-0").hide();
+}
 
 //adapted from: https://spin.atomicobject.com/2014/01/21/convert-svg-to-png/
 function svgImage(xml) {
@@ -39,55 +39,55 @@ function svgImage(xml) {
 }
 
 
-    function setupPlotsForSearchResults(){
-       //the two blocks below can be factored together.
-       var plottingData = [];
-       $("td.plotting_data" ).each(function() {
-           var data_for_one_plot = this.textContent;
-           onePlotData = jQuery.parseJSON(data_for_one_plot);
-           plottingData.push(onePlotData);
-       });
-       cloneSVGSkeleton(plottingData);
-       $("svg#target-0").hide();
-       $("svg#target-refhalf-0").hide();
-       $("svg#target-snphalf-0").hide();
-       for ( var n = 0; n < plottingData.length; n++){
-           var targetSVGid = "target-" + plottingData[n].plot_id_str;
-           var halfPlotId = 'target-refhalf-' +  plottingData[n].plot_id_str; //change to halfRefPlot
-           var halfSnpPlotId = 'target-snphalf-' +  plottingData[n].plot_id_str;
+function setupPlotsForSearchResults(){
+   //the two blocks below can be factored together.
+   var plottingData = [];
+   $("td.plotting_data" ).each(function() {
+       var data_for_one_plot = this.textContent;
+       onePlotData = jQuery.parseJSON(data_for_one_plot);
+       plottingData.push(onePlotData);
+   });
+   cloneSVGSkeleton(plottingData);
+   $("svg#target-0").hide();
+   $("svg#target-refhalf-0").hide();
+   $("svg#target-snphalf-0").hide();
+   for ( var n = 0; n < plottingData.length; n++){
+       var targetSVGid = "target-" + plottingData[n].plot_id_str;
+       var halfPlotId = 'target-refhalf-' +  plottingData[n].plot_id_str; //change to halfRefPlot
+       var halfSnpPlotId = 'target-snphalf-' +  plottingData[n].plot_id_str;
 
-           //console.log("creating plot for id : " + targetSVGid);
-           makeAPlot(plottingData[n], targetSVGid);
-           makeAHalfPlot(plottingData[n], halfPlotId );
-           makeAHalfPlotSNP(plottingData[n], halfSnpPlotId );
-           if ( n > 0 ){
-             //console.log("adding hidden class to " + targetSVGid);
-             $("#"+targetSVGid).parent().addClass("hidden");
-           }else {
-             $("#save_plot").attr('style', 'display: inline;');
-             $("#"+targetSVGid).parent().addClass("show-plot");
-           } 
-           var plotToMove = $("#"+targetSVGid).parent().detach();
-           var halfPlotToMove = $("#" + halfPlotId).parent().detach();
-           var halfSnpPlotToMove = $("#" + halfSnpPlotId).parent().detach();
-
-           var idOfPlotTarget = "#" + targetSVGid.replace('target-', 'show_plot_');
-           var idOfHalfPlotTarget = "#ref-plot-" + plottingData[n].plot_id_str;
-           var idOfHalfSnpPlotTarget = "#snp-plot-" + plottingData[n].plot_id_str;
-
-           var putPlotHere = $(idOfPlotTarget).parent();
-           var putHalfPlotHere = $(idOfHalfPlotTarget);
-           var putHalfSnpPlotHere = $(idOfHalfSnpPlotTarget);
-
-           //console.log("trying to put half-plot " + putHalfPlotHere);
-           console.log(putHalfPlotHere);
-           plotToMove.appendTo(putPlotHere);
-
-           halfPlotToMove.appendTo(putHalfPlotHere);
-           halfSnpPlotToMove.appendTo(putHalfSnpPlotHere);
-
-           halfPlotToMove.find('svg').show();
-           halfSnpPlotToMove.find('svg').show();
+       //console.log("creating plot for id : " + targetSVGid);
+       makeAPlot(plottingData[n], targetSVGid);
+       makeAHalfPlot(plottingData[n], halfPlotId );
+       makeAHalfPlotSNP(plottingData[n], halfSnpPlotId );
+       if ( n > 0 ){
+         //console.log("adding hidden class to " + targetSVGid);
+         $("#"+targetSVGid).parent().addClass("hidden");
+       }else {
+         $("#save_plot").attr('style', 'display: inline;');
+         $("#"+targetSVGid).parent().addClass("show-plot");
        } 
-       console.log("completed plotting!");
-      }
+       var plotToMove = $("#"+targetSVGid).parent().detach();
+       var halfPlotToMove = $("#" + halfPlotId).parent().detach();
+       var halfSnpPlotToMove = $("#" + halfSnpPlotId).parent().detach();
+
+       var idOfPlotTarget = "#" + targetSVGid.replace('target-', 'show_plot_');
+       var idOfHalfPlotTarget = "#ref-plot-" + plottingData[n].plot_id_str;
+       var idOfHalfSnpPlotTarget = "#snp-plot-" + plottingData[n].plot_id_str;
+
+       var putPlotHere = $(idOfPlotTarget).parent();
+       var putHalfPlotHere = $(idOfHalfPlotTarget);
+       var putHalfSnpPlotHere = $(idOfHalfSnpPlotTarget);
+
+       //console.log("trying to put half-plot " + putHalfPlotHere);
+       console.log(putHalfPlotHere);
+       plotToMove.appendTo(putPlotHere);
+
+       halfPlotToMove.appendTo(putHalfPlotHere);
+       halfSnpPlotToMove.appendTo(putHalfSnpPlotHere);
+
+       halfPlotToMove.find('svg').show();
+       halfSnpPlotToMove.find('svg').show();
+   } 
+   console.log("completed plotting!");
+  }
