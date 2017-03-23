c3fd97acbf97b3666dc209ce5e40c5f6816aef74
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2017-03-23 14:08:45 +0000

    Add the option of an inequality direction changer to two of the SNPids to SNPid window searches.

--- ss_viewer/views/snpid_window_search.py
+++ ss_viewer/views/snpid_window_search.py
@@ -4,24 +4,18 @@ from django.http import HttpResponse
 import json
 
 class SnpidWindowSearchView(GenericSearchView):
-    #TODO: adapt all views to work like this..
-    #TODO: consider: changing the GenericSearchView to GenericAjaxySearchView.
     form_class = SearchBySnpidWindowForm
     form_name_in_context = 'snpid_window_form'
     api_action_name = 'search-by-window-around-snpid'
   
-    #def post(self, request, *args, **kwargs):
-    #    print "What's in the post, AJAX?"
-    #    print "request.POST: " + str(request.POST)
-    #    response_data = { 'one key' : 'one value' }
-    #    return HttpResponse(json.dumps(response_data),
-    #                        content_type="application/json"
-    #                        )
-  
     def setup_api_search_query(self, form_data, request):
         requested_snpid = form_data['snpid']
+
+        pvalue_snp_direction = form_data['pvalue_snp_direction']
+        
         api_search_query =  {'snpid'       : requested_snpid, 
-                             'window_size' : form_data['window_size'] }
+                             'window_size' : form_data['window_size'],
+                             'pvalue_snp_direction' : pvalue_snp_direction  }
         api_search_query.update(self.get_pvalues_from_form())
         return api_search_query
 
