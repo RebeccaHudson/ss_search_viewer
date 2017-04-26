ceb263e057024fc8604d0dc4f3e4968c3ac30ce6
Rebecca Hudson <rhudson@atsnp.biostat.wisc.edu>
2017-04-26 23:58:10 +0000

    Remove the default database; this project connects to Elasticsearch instead of using a regular DB connector.

--- ss_search_viewer/settings.py
+++ ss_search_viewer/settings.py
@@ -89,13 +89,13 @@ WSGI_APPLICATION = 'ss_search_viewer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+#
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
