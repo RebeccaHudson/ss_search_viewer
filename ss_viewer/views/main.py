c0bd140fb156d20ea16a88c074a0ce08790c6921
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2017-05-01 18:01:05 +0000

    Change test_svg_plots to load a couple of test plots worth of plotting data into the context of the template.

--- ss_viewer/views/main.py
+++ ss_viewer/views/main.py
@@ -7,29 +7,40 @@ from ss_viewer.views.shared import MotifPlottingData
 def index(request):
   return HttpResponse("Try another url, like :ss_viewer/multi-search.")
 
+#
 def test_svg_plots(request):
     name_of_template  =  'ss_viewer/show_test_plot.html' 
-    path_to_tests = '/test_data/plotsToTest.json'
-    testData = None
-    fpath = os.path.dirname(os.path.dirname(__file__)) + path_to_tests
-    with open(fpath,  'r') as f:
-        testjson = json.load(f)
-        testdata = testjson['hits']
-        print "testjson hits " + str(testdata.keys())
-        testdata2 = testdata['hits'][2]['_source']
-        print "testjson2 hits " + str(testdata2.keys())
+    #path_to_tests = '/test_data/plotsToTest.json'
+    test_plot_dir = '/test_data/'
+    plots_to_test = ['shorter_test_plot.json', 'normal_test_plot.json']
+    test_data = {}
+    #load up each file of test data.
+    for one_plot in plots_to_test:
+        path = '/'.join([test_plot_dir, one_plot])
+        #fpath = os.path.dirname(os.path.dirname(__file__)) + path_to_tests
+        fpath = os.path.dirname(os.path.dirname(__file__)) + path
+        with open(fpath,  'r') as f:
+            #testjson = json.load(f)
+            #testdata = testjson['hits']
+            dict_key = one_plot.replace('.json', '')
+            test_data[dict_key] = json.dumps(json.load(f))
+            #testdata = testjson
+            #print "testjson hits " + str(testdata.keys())
+            #testdata2 = testdata['hits'][2]['_source']
+            #print "testjson2 hits " + str(testdata2.keys())
+    #get a dictionary.
+    print "test data: " + repr(test_data)
+    #now included w/ the test data. motif = testdata2['motif']   
+    #motifMap = MotifPlottingData()
+    #plot_id_string = 'MA0002.2_rs538221432_T'
+    #motif_data = motifMap.lookup_motif_data(motif)
+    #print "motif data : " + str(motif_data)
 
-    motif = testdata2['motif']   
-    motifMap = MotifPlottingData()
-    plot_id_string = 'MA0002.2_rs538221432_T'
-
-    motif_data = motifMap.lookup_motif_data(motif)
-    print "motif data : " + str(motif_data)
-
-    context =  {'dynamic_svg_data' : 'scooby doo',
-                 "happy_data" : json.dumps(testdata2),
-                 "motif_data" : json.dumps(motifMap.lookup_motif_data(motif)) }
-                    
+    #motif_data is now included.
+    context = {}# { 'dynamic_svg_data' : 'scooby doo',
+                # "happy_data" : json.dumps(testdata2),
+                # "motif_data" : json.dumps(motifMap.lookup_motif_data(motif)) }
+    context.update(test_data)
     return render(request, name_of_template, context)
 
 def help_page(request):
