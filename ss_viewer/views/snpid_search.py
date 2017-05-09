403b5fd53ddcbdfdcc5533e4ec798d08498380dc
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2017-05-09 20:54:58 +0000

    Remove the file, if present, from the cleaned form data that is propogated back to the search page for paging and downloads.

--- ss_viewer/views/snpid_search.py
+++ ss_viewer/views/snpid_search.py
@@ -8,4 +8,8 @@ class SnpidSearchView(GenericSearchView):
 
     def setup_api_search_query(self, form_data, request):
         api_search_query = { 'snpid_list' : form_data['snpid_list']  }
+        #should be able to delete the file from the form data at this point.
+        if 'file_of_snpids' in form_data.keys() and \
+            form_data['file_of_snpids'] is not None:
+            del form_data['file_of_snpids']
         return api_search_query
