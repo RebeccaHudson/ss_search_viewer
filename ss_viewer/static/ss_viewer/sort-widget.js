188c16db89027e0e8a72a7b014ab97a7badcfc57
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2017-05-31 14:22:31 +0000

    Change the sort control such that there is one graphic icon alongside each sortable column that inidcates it's sort order.

--- ss_viewer/static/ss_viewer/sort-widget.js
+++ ss_viewer/static/ss_viewer/sort-widget.js
@@ -6,16 +6,18 @@
     jQuery(document).ready(function ($) {
       var sort_order = readSortOrder();
       buildQuery();
-  
+ 
+
+      //TODO: this is now 4 buttons instead of 1 
       $(".sortflip").on('click', function(){
          //flips the sort direction for the currently displayed field.
          //also flips the icon indicating sort order.
          var selected_field = $("div.active .sort-select option:selected");
          if ($(this).hasClass('glyphicon-sort-by-attributes')){
-             showDescendingIcon();
+             showDescendingIcon(this);
              selected_field.attr('direction', 'desc'); 
          }else{ 
-             showAscendingIcon();
+             showAscendingIcon(this);
              selected_field.attr('direction', 'asc'); 
           }
           buildQuery();
@@ -24,21 +26,26 @@
       //only hits the first active div.
       //$("div.active .sort-select option").on('click', function(){
       $(".sort-select option").on('click', function(){
-       var drctn = $(this).attr('direction');
-       if (drctn == 'asc' ){ showAscendingIcon(); }else{ 
-          showDescendingIcon();
-       }
+       //this is no longer needed, as far as I can tell.
+       /*var drctn = $(this).attr('direction');
+       if (drctn == 'asc' ){ showAscendingIcon(this); }else{ 
+          showDescendingIcon(this);
+       }*/
       });
 
     } );//end of document.ready stuff here.
 
-    function showAscendingIcon(){
+    function showAscendingIcon(target){
        var selector = $("div.active .sortflip");
+       selector = $(target);
+       //console.log("show ascending");
        selector.removeClass('glyphicon-sort-by-attributes-alt');
        selector.addClass('glyphicon-sort-by-attributes');
     }
-    function showDescendingIcon(){
+    function showDescendingIcon(target){
        var selector = $("div.active .sortflip");
+       //console.log("show descending");
+       selector = $(target);
        selector.removeClass('glyphicon-sort-by-attributes');
        selector.addClass('glyphicon-sort-by-attributes-alt');
     }
@@ -61,13 +68,26 @@
      if ( (el0 == null) || (el1 == null) ){ return; }
      var i1 = el0.index; var i2 = el1.index;
      el0.parentNode.insertBefore(el1, el0);
+     //take the value that's in the i1th sort order graphic, 
+     //and put it into the i2th soirt order graphic and vice versa.
+     //
+     //CLEAN UP THE FOLLOWING CODE:
+     var tempSwap = $("div.active .sortflip")[i1]; //pull the sort order out of this.
+     console.log(tempSwap);
+     tempSwap = $(tempSwap);
+     console.log(tempSwap.attr('class'));
+     var tempSwapClass = tempSwap.attr('class');
+     var swapB =  $("div.active .sortflip")[i2];
+     tempSwap.attr('class', $(swapB).attr('class')); 
+     $(swapB).attr('class', tempSwapClass);
    }
 
    function check_for_end(to_swap){
        if (to_swap.length > 0){ return to_swap[0]; }
        return null;
    }
- 
+
+   //TODO need to make the sort order graphics reflect the change in order.
    function priorityChange(event, up_or_down){
       event.preventDefault();
       var  active_form = $("#tabbed-forms div.active div.sort-controls");
@@ -76,11 +96,19 @@
         return; 
       }
       selected = selected[0];
+      console.log("selected");
+      console.log(selected);
+      var index_of_selected = $(selected).index(); //gives index of selected element prior to move.
+      var index_of_swapped;
       var to_swap;
       if (up_or_down == 'down'){ 
+        index_of_swapped = index_of_selected + 1;
+        console.log("index of downswapped " + index_of_swapped);
         to_swap = check_for_end(active_form.find(".sort-select").find(":selected").next());
         swapAdjacent(selected, to_swap); 
       } else {
+        index_of_swapped = index_of_selected - 1;
+        console.log("index of upswapped " + index_of_swapped);
         to_swap = check_for_end(active_form.find(".sort-select").find(":selected").prev());
         swapAdjacent(to_swap, selected); 
       }
