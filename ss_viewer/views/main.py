4c8d4eb339d264237027b3dec195ca12a8419a32
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2017-06-20 00:38:14 +0000

    Delete a bunch of comments and references to the now-Deprecated MotifPlottingData.

--- ss_viewer/views/main.py
+++ ss_viewer/views/main.py
@@ -2,44 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 import os
-from ss_viewer.views.shared import MotifPlottingData
+#from ss_viewer.views.shared import MotifPlottingData
 
 def index(request):
   return HttpResponse("Try another url, like :ss_viewer/multi-search.")
 
-#
 def test_svg_plots(request):
     name_of_template  =  'ss_viewer/show_test_plot.html' 
-    #path_to_tests = '/test_data/plotsToTest.json'
     test_plot_dir = '/test_data/'
     plots_to_test = ['shorter_test_plot.json', 'normal_test_plot.json']
     test_data = {}
     #load up each file of test data.
     for one_plot in plots_to_test:
         path = '/'.join([test_plot_dir, one_plot])
-        #fpath = os.path.dirname(os.path.dirname(__file__)) + path_to_tests
         fpath = os.path.dirname(os.path.dirname(__file__)) + path
         with open(fpath,  'r') as f:
-            #testjson = json.load(f)
-            #testdata = testjson['hits']
             dict_key = one_plot.replace('.json', '')
             test_data[dict_key] = json.dumps(json.load(f))
-            #testdata = testjson
-            #print "testjson hits " + str(testdata.keys())
-            #testdata2 = testdata['hits'][2]['_source']
-            #print "testjson2 hits " + str(testdata2.keys())
     #get a dictionary.
     print "test data: " + repr(test_data)
-    #now included w/ the test data. motif = testdata2['motif']   
-    #motifMap = MotifPlottingData()
-    #plot_id_string = 'MA0002.2_rs538221432_T'
-    #motif_data = motifMap.lookup_motif_data(motif)
-    #print "motif data : " + str(motif_data)
 
     #motif_data is now included.
-    context = {}# { 'dynamic_svg_data' : 'scooby doo',
-                # "happy_data" : json.dumps(testdata2),
-                # "motif_data" : json.dumps(motifMap.lookup_motif_data(motif)) }
+    context = {}
+                
     context.update(test_data)
     return render(request, name_of_template, context)
 
