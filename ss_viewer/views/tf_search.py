7652e25f08ba658840e7ecf421322d9c3d87d233
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2017-06-21 17:00:24 +0000

    Use the same logic and mechanism for searches by either ENCODE or JASPAR transcription factor.

--- ss_viewer/views/tf_search.py
+++ ss_viewer/views/tf_search.py
@@ -10,16 +10,20 @@ class TranscriptionFactorSearchView(GenericSearchView):
 
     #do transcription-factor specific stuff here. 
     def setup_api_search_query(self, form_data):
-        tft = TFTransformer()
-        motif_value = None
+        tft = None; tf_for_search = None
         if form_data['tf_library'] == 'encode':
+            tft = TFTransformer('encode')
             #The transcription factor IS the motif's prefix; search by that.
-            motif_value = form_data['encode_trans_factor'] 
+            tf_for_search = form_data['encode_trans_factor']
         else:
             #Use JASPAR motifs.
             trans_factor = form_data['trans_factor']
-            motif_value = tft.lookup_motifs_by_tf(form_data['trans_factor'])
-            print "motif value for not download " + repr(motif_value)
+            tft = TFTransformer('jaspar')
+            tf_for_search =  form_data['trans_factor'] 
+
+        motif_value = tft.lookup_motifs_by_tf(tf_for_search)
+        print "motif value for not download " + repr(motif_value)
+        #Downloads already have the query saved on the page?
         api_search_query = {'motif'      :  motif_value, 
                             'tf_library' :  form_data['tf_library'] }
         return api_search_query
