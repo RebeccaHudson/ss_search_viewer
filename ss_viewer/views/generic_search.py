e90ec043b02d6af5ae8437ee75b5cc4cac6289c2
Rebecca Hudson <rebeccakathrynhudson@gmail.com>
2017-12-07 19:52:38 +0000

    Clean up handling of paging requests and download requests.

--- ss_viewer/views/generic_search.py
+++ ss_viewer/views/generic_search.py
@@ -7,7 +7,6 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
 from django.views.generic import View
 from ss_viewer.views.shared import StandardFormset
-from ss_viewer.views.shared import PValueDictFromForm
 from ss_viewer.views.shared import Paging
 from ss_viewer.views.shared import StreamingCSVDownloadHandler
 from ss_viewer.forms import SharedSearchControlsForm 
@@ -31,32 +30,25 @@ class GenericSearchView(View):
            request.POST = json.loads(st)
         return request 
 
+    #try to flatten one more time...
     def setup_form_for_paging_or_download_request(self,request):
         newDict = None
-        self.shared_search_controls_dict = {} 
-        post_dict_to_use = None
-        #maybe should undo this? might not be needed.
-        if type(request.POST) is dict:
-            #occurs with downloads
-            post_dict_to_use = request.POST
-        else: 
-            #occurs with paging requests
-            post_dict_to_use = request.POST.dict()
-        #print "right before assignment: " + repr(request.POST.dict())
-        print "right before assignment: " + repr(post_dict_to_use)
-        for x in [ 'page_of_results_shown', 'ic_filter', 'sort_order', 'pvalue_snp', 'pvalue_ref', 'pvalue_rank', \
-                    'pvalue_ref_direction' , 'pvalue_snp_direction' ]:
-            self.shared_search_controls_dict[x] = post_dict_to_use[x]
-        self.shared_search_controls_dict['sort_order'] =  json.loads(self.shared_search_controls_dict['sort_order'])
-        self.shared_search_controls_dict['ic_filter'] =  json.loads(self.shared_search_controls_dict['ic_filter'])
         if request.POST['action'] in ['Prev', 'Next']:
+            self.shared_search_controls_dict = copy.deepcopy(request.POST.dict())
             newDict = self.setup_form_for_paging_request(request)
-        else:
+        else: 
+            self.shared_search_controls_dict = copy.deepcopy(request.POST)
             newDict = self.setup_form_for_download_request(request)
-        newDict = self.add_form_prefix_to_fields_for_search(newDict)
+
+        #print "right before assignment: " + repr(post_dict_to_use)
+        for one_field in ['sort_order', 'ic_filter']: 
+            self.shared_search_controls_dict[one_field] =  \
+              json.loads(self.shared_search_controls_dict[one_field])
+
+        newDict = self.add_form_prefix_to_fields_for_search(newDict)  #is this still needed?
         self.search_form = self.form_class(newDict)
 
-    # Don't call this for paging requests.
+    # Don't call this for paging or download requests, it comes in with all the other keys.
     def setup_shared_search_controls_form(self, request):
         self.shared_search_controls_dict = request.POST.dict()['shared_controls']
         self.shared_search_controls_dict = json.loads(self.shared_search_controls_dict)
@@ -82,8 +74,9 @@ class GenericSearchView(View):
 
     #Must include some handling for shared_search_controls.
     def pull_all_query_data_from_form(self):
-        form_data = self.search_form.cleaned_data
+        form_data = self.search_form.cleaned_data  #query-type specific.
         self.api_search_query = self.setup_api_search_query(form_data)
+
         self.api_search_query.update(self.get_pvalues_from_form())
         self.api_search_query.update(self.get_pvalue_directions_from_form() ) 
         self.api_search_query.update(self.handle_sort_order() ) 
@@ -177,21 +170,12 @@ class GenericSearchView(View):
         return pvalue_directions
 
     def get_pvalues_from_form(self):
-        pvalues_for_search = {}
-        pvalue_dict = None
-        pvalue_dict = PValueDictFromForm.get_pvalues_from_dict(self.shared_search_controls_dict) 
-        pvalues_for_search['pvalue_rank'] = pvalue_dict['pvalue_rank_cutoff']
-        if 'pvalue_ref_cutoff' in pvalue_dict:
-            pvalues_for_search.update({'pvalue_ref'  : pvalue_dict['pvalue_ref_cutoff']})
-        if 'pvalue_snp_cutoff' in pvalue_dict:
-            pvalues_for_search.update({'pvalue_snp'  : pvalue_dict['pvalue_snp_cutoff']})
-
-        #TODO: clean this up!
-        if 'pvalue_ref' in pvalue_dict:
-            pvalues_for_search.update({'pvalue_ref'  : pvalue_dict['pvalue_ref']})
-        if 'pvalue_snp' in pvalue_dict:
-            pvalues_for_search.update({'pvalue_snp'  : pvalue_dict['pvalue_snp']})
-        return pvalues_for_search
+        print "in get_pvalues_from_form " + repr(self.shared_search_controls_dict)
+        pval_dict = {}
+        for one_pval in ['rank', 'ref', 'snp']:
+            k = '_'.join(['pvalue', one_pval])
+            pval_dict[k] = self.shared_search_controls_dict[k]
+        return pval_dict
 
     def handle_invalid_form(self):
         errs = self.search_form.errors
